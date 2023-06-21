96e68966e8f3d934020bc4b50a73eb8d2310cd04
luka.danger <lukathevegan@gmail.com>
2023-06-21 17:25:53 -0600

    Add more array methods

--- js-basics/practice.js
+++ js-basics/practice.js
@@ -1,8 +1,7 @@
 // Array 1
 const fruits = ["apples", "bananas", "oranges"];
 fruits.unshift("watermelon");
-fruits.pop()
-console.log(fruits.join(" are fruits "));
+console.log("I bought " + fruits.join(" and "));
 
 // Array Option 2
 const fruitList = [
@@ -16,14 +15,20 @@ fruitList[0] = "mango"
 // Add grapes to end of list
 fruitList[3] = "grapes"
 
+bigList = fruits.concat(fruitList);
+console.log(bigList)
+
 // Implement method to insert items to a specific index of array
 Array.prototype.insert = function (index, items) {
     this.splice(index, 0, items);
 };
 // Add and between bananas and oranges
-fruits.insert(2, "and")
-let fruitString = fruits.toString();
-console.log(`My favorite fruits are ${fruitString}`)
+bigList.insert(7, " and ")
+console.log(`My favorite fruits are ${bigList}`)
+
+// 1 is index, 0 is how many elements are removed
+fruits.splice(1, 0, "tomato", "green beans")
+console.log(`Grocery List: ${fruits}`)
 
 // Object 1
 let waffles = { name: "Waffles", species: "Dog", age: 4, isCute: true }
