32d5cffac9323b20db4a0bbf75c3ae2773010065
luka.danger <lukathevegan@gmail.com>
2023-06-21 17:59:21 -0600

    Add for...of loop

--- js-basics/arrays-practice.js
+++ js-basics/arrays-practice.js
@@ -26,6 +26,7 @@ Array.prototype.insert = function (index, items) {
 // Add and between bananas and oranges
 bigList.insert(7, " and ")
 console.log(`My favorite fruits are ${bigList}`)
+bigList.splice(7, 1)
 
 // 1 is index, 0 is how many elements are removed
 fruits.splice(1, 0, "tomato", "green beans")
@@ -53,4 +54,8 @@ for (i = 1; i <= fruitList.length; i++) {
     fruit = fruitList[i - 1];
     newFruitList.push(fruit);
     console.log(`The fruitList array contains ${i} fruits: ` + newFruitList)
+}
+
+for (fruit of bigList) {
+    console.log(fruit);
 }
