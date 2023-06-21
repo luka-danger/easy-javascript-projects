f5d1896360b62617f89da7495b1aa94688a4d7ba
luka.danger <lukathevegan@gmail.com>
2023-06-21 12:54:19 -0600

    Add array

---
+++ js-basics/practice.js
@@ -0,0 +1,12 @@
+// Array 1
+const fruits = ["apple", "banana", "orange"];
+
+// Array Option 2
+const fruitList = [
+    "kiwi",
+    "pineapple",
+    "cherry"
+]
+
+let fruit = fruits[1]
+console.log(fruit)
