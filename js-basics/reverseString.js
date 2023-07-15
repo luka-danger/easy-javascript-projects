03baa01a2bc614e06367578ce6b2765c9ec9fcad
luka-danger <lukathevegan@gmail.com>
2023-07-15 14:46:32 -0600

    Remove comments

--- js-basics/reverseString.js
+++ js-basics/reverseString.js
@@ -1,7 +1,3 @@
-/*let word = "nice"
-let numOfWords = word.length 
-console.log(word[numOfWords - 1])*/ 
-
 const reverseString = function(word) {
     let reversed = ""; 
     for (let i = word.length - 1; i >= 0; i--) {
