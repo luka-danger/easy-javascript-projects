5c713d77a240072e9567fc09f235ae1015c27a21
luka-danger <lukathevegan@gmail.com>
2023-07-15 14:39:37 -0600

    Add for loop

--- js-basics/reverseString.js
+++ js-basics/reverseString.js
@@ -4,4 +4,10 @@ console.log(word[numOfWords - 1])*/
 
 const reverseString = function(word) {
     let reversed = ""; 
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversed += i
+    }
+    console.log(reversed); 
 } 
+
+reverseString("hello")
