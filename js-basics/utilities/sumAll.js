a1e3bad52f04f4ca3425098074cd95b4eb6041e6
luka.danger <lukathevegan@gmail.com>
2023-07-18 22:24:09 -0600

    Add addition assignment to for loop

--- js-basics/utilities/sumAll.js
+++ js-basics/utilities/sumAll.js
@@ -1,6 +1,9 @@
 const sumAll = function (num1, num2) {
     let sumsOfNumber = 0;
     for (let i = num1; i <= num2; i++) {
-
+        sumsOfNumber += i;
     }
-}; 
+    return sumsOfNumber;
+};
+
+console.log(sumAll(1, 4))
