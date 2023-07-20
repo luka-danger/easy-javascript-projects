424089fdfd856a68c84d04ef2f334bf2f3c0b9ca
luka-danger <lukathevegan@gmail.com>
2023-07-19 22:13:18 -0600

    Create convertToCelcius() function

--- js-basics/utilities/tempConversion.js
+++ js-basics/utilities/tempConversion.js
@@ -1,5 +1,6 @@
-const convertToCelsius = function () {
-
+const convertToCelsius = function (temp) {
+    let tempInCelcius = (temp - 32) * 5/9;
+    return tempInCelcius; 
 }; 
 
 const convertToFahrenheit = function () {
