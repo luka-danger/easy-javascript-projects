de15e813bb013ea8ac47e6fc1384cbaeb949810d
luka-danger <lukathevegan@gmail.com>
2023-07-19 22:23:25 -0600

    Add new functions for Kelvin

--- js-basics/utilities/tempConversion.js
+++ js-basics/utilities/tempConversion.js
@@ -1,8 +1,29 @@
-const convertToCelsius = function (temp) {
+const convertToCelsius = function(temp) {
     let tempInCelcius = (temp - 32) * 5/9;
     return tempInCelcius; 
 }; 
 
-const convertToFahrenheit = function () {
+const convertToFahrenheit = function(temp) {
+    let tempInFahrenheit = (temp * (9/5) + 32); 
+    return tempInFahrenheit; 
+}; 
 
+const celciusToKelvin = function(temp) {
+    let celToKel = ; 
+    return celToKel; 
+};
+
+const kelvinToCelcius = function(temp) {
+    let kelToCel = ;
+    return kelToCel; 
+};
+
+const fahrenheitToKelvin = function(temp) {
+    let fahrToKel = ;
+    return fahrToKel = ;
+};
+
+const kelvinToFahrenheit = function(temp) {
+    let kelToFahr = ;
+    return kelToFahr; 
 }; 
