d33572c514f7a7003f687f771f719a7b14e69a42
luka-danger <lukathevegan@gmail.com>
2023-10-17 13:16:48 -0600

    Add comments
    Comments analyze run time complexity for algorithm

--- js-basics/algorithms/big-o.js
+++ js-basics/algorithms/big-o.js
@@ -1,7 +1,14 @@
 const data = ['A', 'B', 'C']; 
 
+// Algorithm 1 
 // Print each element in data array
 for (let i=0; i < data.length; i++) {
+    // Constant for algorithm
+    // Runs over and over to get result 
     console.log(data[i]); 
 }; 
 
+/* Time Complexity for Algorithm 1: 
+As n grows, algorithm scales 1:1 with size of n
+Big O: O(n) - Linear Complexity 
+*/
