8edba729772127ef3f233044d6577ddd3dd20069
luka-danger <lukathevegan@gmail.com>
2023-10-17 13:19:29 -0600

    Create new algorithm and new array

--- js-basics/algorithms/big-o.js
+++ js-basics/algorithms/big-o.js
@@ -1,4 +1,5 @@
-const data = ['A', 'B', 'C']; 
+const data = ['A', 'B', 'C']
+const data2 = [1, 2, 3, 4, 5]
 
 // Algorithm 1 
 // Print each element in data array
@@ -12,3 +13,11 @@ for (let i=0; i < data.length; i++) {
 As n grows, algorithm scales 1:1 with size of n
 Big O: O(n) - Linear Complexity 
 */
+
+//Algorithm 2
+for (let j = 0; j < data2.length; j++) {
+    console.log(data2[j]);
+}; 
+for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+}; 
