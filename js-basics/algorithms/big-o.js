b104bd26a828e3e0dfd27c959549e72845690925
luka-danger <lukathevegan@gmail.com>
2023-10-17 13:34:22 -0600

    Add Algo 3

--- js-basics/algorithms/big-o.js
+++ js-basics/algorithms/big-o.js
@@ -24,5 +24,18 @@ for (let i = 0; i < data.length; i++) {
 
 /* Time Complexity for Algorithm 2:
 Algorithm scales based on length of 2 separate arrays
-O(n + a)
+Big O: O(n + a)
+*/ 
+
+//Algorithm 3
+for (let j = 0; j < data2.length; j++) {
+    for (let i = 0; i < data.length; i++) 
+        console.log(data[i] + data2[j];);
+}; 
+
+/* Time Complexity for Algorithm 3:
+Loops through every element of data2 and for every element 
+of data2 it also loops through data 
+n times for every single a time in loop
+Big O: O(n * a)
 */ 
