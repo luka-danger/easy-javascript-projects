19830728ae204a90ae61ba1e810409e8f5cd16de
luka-danger <lukathevegan@gmail.com>
2023-12-04 21:35:09 -0700

    Initial commit
    First node module, woo

---
+++ node/first-app/logger.js
@@ -0,0 +1,9 @@
+// can send http requests to url to log messages in cloud
+
+// send http request to this endpoint 
+let url = 'http://mylogger.io/log'
+
+function log(message) {
+    // Send HTTP request 
+    console.log(message)
+}
