e6611bcd3a4989188df454da892055f7442ab7a4
luka-danger <lukathevegan@gmail.com>
2023-12-05 11:47:39 -0700

    Add all modules

--- node/first-app/app.js
+++ node/first-app/app.js
@@ -5,3 +5,15 @@ let freeMemory = os.freemem()
 
 console.log(`Total Memory: ${totalMemory}`)
 console.log(`Free Memory: ${freeMemory}`)
+
+// main module 
+// Use require to load the function 
+// Store value in constant so we don't accidentally override
+const logger = require('./logger');
+
+logger()
+// Show file path
+const path = require('node:path');
+
+let pathObject = path.parse(__filename);
+console.log(pathObject)
