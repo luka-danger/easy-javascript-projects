322f6afd781710a8fd2e056b0b6fcea3cb8ab1f4
luka-danger <lukathevegan@gmail.com>
2023-12-24 18:19:20 -0700

    Create factorial function and test cases

--- js-basics/utilities/calculator/calculator.spec.js
+++ js-basics/utilities/calculator/calculator.spec.js
@@ -59,4 +59,26 @@ describe('power', () => {
         expect(calculator.power(2,3)).toBe(8);
     });
 }); 
+
+describe('factorial', () => {
+    test('computes the factorial of 0', () => {
+      expect(calculator.factorial(0)).toBe(1); // 0! = 1
+    });
+  
+    test('computes the factorial of 1', () => {
+      expect(calculator.factorial(1)).toBe(1);
+    });
+  
+    test('computes the factorial of 2', () => {
+      expect(calculator.factorial(2)).toBe(2);
+    });
+  
+    test('computes the factorial of 5', () => {
+      expect(calculator.factorial(5)).toBe(120);
+    });
+  
+    test('computes the factorial of 10', () => {
+      expect(calculator.factorial(10)).toBe(3628800);
+    });
+  });
   
