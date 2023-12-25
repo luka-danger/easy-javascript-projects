18a3c34986167964da4f003dc275944b24cd3fee
luka-danger <lukathevegan@gmail.com>
2023-12-24 18:44:53 -0700

    Delete all files

--- js-basics/utilities/reverseString.js
+++
@@ -1,9 +0,0 @@
-const reverseString = function(word) {
-    let reversed = ""; 
-    for (let i = word.length - 1; i >= 0; i--) {
-        reversed += word[i];
-    }
-    console.log(reversed); 
-} 
-
-reverseString("hello")
