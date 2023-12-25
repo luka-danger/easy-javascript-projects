e382e771551f0c87be84f5e9ac33731e48c9f3a5
luka-danger <lukathevegan@gmail.com>
2023-12-25 12:18:43 -0700

    Create algo
    For loop and if statement

--- js-basics/utilities/palindrome.js
+++ js-basics/utilities/palindrome.js
@@ -1,3 +1,11 @@
-const palindromes = function () {
-
+const palindrome = function (word) {
+    let reverseWord = []
+    for (letter in word) {
+        reverseWord.add(letter)
+    }
+    if (reverseWord === word) {
+        return true;
+    } else {
+        return false; 
+    }
 };
