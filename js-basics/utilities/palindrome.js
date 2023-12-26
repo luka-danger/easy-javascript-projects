a4fa9438d7602316b040e1f9037662d7adb0f3c1
luka-danger <lukathevegan@gmail.com>
2023-12-26 05:23:11 -0700

    Fix algo

--- js-basics/utilities/palindrome.js
+++ js-basics/utilities/palindrome.js
@@ -1,6 +1,10 @@
+// .split() splits each character into array
+// .reverse() reverses the direction of the array
+// .join() puts characters into 'word' variable as single word 
+
+
 const palindromes = function (word) {
-    // .split() splits each character into array
-    // .reverse() reverses the direction of the array
-    // .join() puts characters into 'word' variable as single word 
-    return word == word.split('').reverse().join('');
+     let noSpace = word.replace(/\s+/g, '').toLowerCase();
+     let newWord = noSpace.split('').reverse().join(''); 
+     return noSpace === newWord;
 };
