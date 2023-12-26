be128b873df79ad069bb6f47ad05a371a3b39f8b
luka-danger <lukathevegan@gmail.com>
2023-12-26 05:29:27 -0700

    Change to arrow function and fix algo
    The creation of the noSpace variable removes all whitespace between text and replaces with empty quotations. The text is also made case insensitive. The original word variable then splits the text into an array by individual character, reverses the characters, and then rejoins the characters before returning the results

--- js-basics/utilities/palindrome.js
+++ js-basics/utilities/palindrome.js
@@ -1,10 +1,9 @@
-// .split() splits each character into array
-// .reverse() reverses the direction of the array
-// .join() puts characters into 'word' variable as single word 
-
-
-const palindromes = function (word) {
-     let noSpace = word.replace(/\s+/g, '').toLowerCase();
-     let newWord = noSpace.split('').reverse().join(''); 
-     return noSpace === newWord;
+const palindromes = (word) => {
+    //
+    let noSpace = word.replace(/\s+/g, '').toLowerCase();
+    // .split() splits each character into array
+    // .reverse() reverses the direction of the array
+    // .join() puts characters into 'word' variable as single word 
+    let originalWord = noSpace.split('').reverse().join(''); 
+    return noSpace === originalWord;
 };
