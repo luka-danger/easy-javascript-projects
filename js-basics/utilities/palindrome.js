a04d7d3da8388d421cdb7b7e900a947879946c0f
luka-danger <lukathevegan@gmail.com>
2023-12-26 05:52:01 -0700

    Change function name
    And move documentation to top

--- js-basics/utilities/palindrome.js
+++ js-basics/utilities/palindrome.js
@@ -1,9 +1,15 @@
-const palindromes = (word) => {
-    //
+// Methods Documentation: 
+
+// '/' starts expression 
+// \s represents any whitespace
+// + represents multiple instances of \s
+// /g represents 'global' (look for all instances in entire string)
+// .split() splits each character into array
+// .reverse() reverses the direction of the array
+// .join() puts characters into 'word' variable as single word 
+
+const palindromeChecker = (word) => {
     let noSpace = word.replace(/\s+/g, '').toLowerCase();
-    // .split() splits each character into array
-    // .reverse() reverses the direction of the array
-    // .join() puts characters into 'word' variable as single word 
-    let originalWord = noSpace.split('').reverse().join(''); 
-    return noSpace === originalWord;
-};
+    let reverseWord = noSpace.split('').reverse().join(''); 
+    return noSpace === reverseWord;
+}; 
