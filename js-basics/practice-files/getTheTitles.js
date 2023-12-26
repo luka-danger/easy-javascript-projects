b4f2a548cd2b9b273eec4df68bb289dc44f19542
luka-danger <lukathevegan@gmail.com>
2023-12-25 18:44:46 -0700

    Add book class

--- js-basics/practice-files/getTheTitles.js
+++ js-basics/practice-files/getTheTitles.js
@@ -1,3 +1,14 @@
 let getTheTitle = function() {
     return Array.map((book) => book.title)
 }; 
+
+const books = [
+    {
+     title: 'Harry Potter',
+     author: 'JK Rowling'   
+    },
+    {
+     title: '1984',
+     author: 'George Orwell'
+    }
+];
